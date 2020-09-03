8a2d69d6e849e62fe4bac1652b66dcf25a4a171b
Mionder <49869397+Mionder@users.noreply.github.com>
2020-09-03 13:55:50 +0300

    header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import "./header.css";
+import CloseIcon from '@material-ui/icons/Close';
+
 
 const Header = () => {
   return (
     <header className="header">
+        <div className="discount_header">
+          <div className="container">
+          <div className="discount_wrapper">
+            <h5 className="discount_info">Product Hunt launch : get a $50 discount until September 13, 2020 😘</h5>
+            <div className="icon_discount">
+              <CloseIcon className="icon_close" />
+            </div>
+          </div>
+          </div>
+      </div>
       <div className="container">
       <nav className="header_wrapper">
         <ul className="header_menu">
+          <li className="header_item logo">
+            <Link to="/">devondemand</Link>
+          </li>
           <li className="header_item">
             <Link to="/">Process</Link>
           </li>
@@ -17,9 +33,11 @@ const Header = () => {
             <Link to="/portfolio">CONTACT</Link>
           </li>
         </ul>
+        <ul className="header_menu">
+          <button className="button_start">Start now</button>
+        </ul>
       </nav>
       </div>
-      <h1>devondemand</h1>
 
     </header>
   )
