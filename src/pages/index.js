5f1e15268e3e7323164b656922ef6733bb9b5104
Arina <45823655+marsiuser@users.noreply.github.com>
2020-09-03 18:35:23 +0300

    arina next

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Layout from "../components/Layout/layout.js";
 import MainDevelopment from "../components/MainDevelopment/mainDevelopment.js";
+import MainProcess from "../components/MainProcess/mainProcess.js";
+import MainShare from "../components/MainShare/mainShare.js";
 
 export default function Home() {
   return (
     <Layout>
       <MainDevelopment />
+      <MainProcess />
+      <MainShare />
     </Layout>
   )
 }
