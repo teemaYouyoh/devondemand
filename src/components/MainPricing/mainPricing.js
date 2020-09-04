05ef16be7d8c7d269bb63488c30d5a5f6d1dfa84
Arina <45823655+marsiuser@users.noreply.github.com>
2020-09-04 11:32:48 +0300

    arina main desktop

---
+++ src/components/MainPricing/mainPricing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./mainPricing.css";
+import checkmark from "./img/checkmark.svg";
+
+const MainPricing = () => {
+  return (
+   <div class="pricing">
+       <div className="container">
+           <div className="pricing-wrapper">
+              <h4>A simple pricing plan</h4>
+              <h5>Fixed fee per page</h5>
+              <div className="pricing-section">
+                <p className="pricing-section__price">$350</p>
+                <ul className="pricing-section__list">
+                    <li><img src={checkmark} alt="checkmark"/> <span>Delivery in 3 business days</span></li>
+                    <li><img src={checkmark} alt="checkmark"/> <span>No contract</span></li>
+                </ul>
+                <p  className="pricing-section__desc">If you don’t have any active job posts you’ll still Have across to your career site and talent pool for no costs.</p>
+                <p  className="pricing-section__footer"><a href="#">Frequently Asked Questions</a> around our pricing.</p>
+              </div>
+           </div>
+       </div>
+   </div>
+  )
+}
+
+export default MainPricing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Layout from "../components/Layout/layout.js";
 import MainDevelopment from "../components/MainDevelopment/mainDevelopment.js";
 import MainProcess from "../components/MainProcess/mainProcess.js";
 import MainShare from "../components/MainShare/mainShare.js";
+import MainJoin from "../components/MainJoin/mainJoin.js";
+import MainPricing from "../components/MainPricing/mainPricing.js";
+import MainNotified from "../components/MainNotified/mainNotified.js";
+import MainIntegrate from "../components/MainIntegrate/mainIntegrate.js";
 
 export default function Home() {
   return (
@@ -10,6 +14,10 @@ export default function Home() {
       <MainDevelopment />
       <MainProcess />
       <MainShare />
+      <MainJoin />
+      <MainPricing />
+      <MainNotified />
+      <MainIntegrate />
     </Layout>
   )
 }
