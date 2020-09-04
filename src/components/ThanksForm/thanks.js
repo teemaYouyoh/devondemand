7d3f64b2ca37fd3923173b3a278244467d7519ac
Mionder <49869397+Mionder@users.noreply.github.com>
2020-09-04 23:43:36 +0300

    form changes

--- src/components/ThanksForm/thanks.js
+++ src/components/ThanksForm/thanks.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState }  from "react";
 import "./thanks.css";
 import Smile from "./img/smile.png";
 import Button from '@material-ui/core/Button';
 const ThanksForm = () => {
+    const [typeProject, setTypeProject] = useState(true);
+    const [isExistProject, setExistProject] = useState(true);
     return(
         <div className="thanks_form">
                 <div className="thanks_form-wrapper">
@@ -11,18 +13,18 @@ const ThanksForm = () => {
                     <p className="thanks_subtitle">Before to start, we have a few questions</p>
                 </div>
                 <div className="form_success">
-                    <form action="POST" className="form">
+                    <div className="form">
                         <h5 className="form_title">Let’s bring your design
                         to life</h5>
                         <label className="label_form" htmlFor="looking_for">You are looking for</label>
                         <div id="looking_for">
-                            <button className="looking_for-active ">normal react app</button>
-                            <button className="form_but">server side render with gatsby</button>
+                            <button onClick={() => setTypeProject(true)} className={typeProject ? "looking_for-active" : "form_but"}>normal react app</button>
+                            <button onClick={() => setTypeProject(false)} className={!typeProject ? "looking_for-active" : "form_but"}>server side render with gatsby</button>
                         </div>
                         <label className="label_form" htmlFor="type_website">Are you developing?</label>
                         <div id="type_website">
-                            <button className="type_website-active">A new website</button>
-                            <button className="form_but">Partialy updated website</button>
+                            <button onClick={() => setExistProject(true)} className={isExistProject ? "type_website-active" : "form_but"}>A new website</button>
+                            <button onClick={() => setExistProject(false)} className={!isExistProject ? "type_website-active" : "form_but"}>Partialy updated website</button>
                         </div>
                         <label className="label_form" htmlFor="api_schema">Link to your API schema (if you have one)</label>
                         <input placeholder="Https://www.myapi.com" type="text" id="api_schema"/>
@@ -30,7 +32,7 @@ const ThanksForm = () => {
                         <label className="label_form" htmlFor="comment">Any comment?</label>
                         <input type="text" placeholder="Any comment?" id="comment"/>
                         <Button className="button_form" variant="contained" color="primary">GET STARTED</Button>
-                    </form>
+                    </div>
                 </div>
         </div>
     )
